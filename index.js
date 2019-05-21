bfab91dbd14833434ebde0648e5c65c79e0fbff3
suprabha94 <suprabha.s.keshavaprakash@gmail.com>
2019-05-21 12:43:36 -0400

    mLab connection

--- index.js
+++ index.js
@@ -4,18 +4,30 @@ const bp = require('body-parser');
 
 require('dotenv').config();
 
+const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose')
 const passport = require('passport')
 
 const app = express();
 
 const port = process.env.PORT || 3000
-const dbPort = process.env.DB_PORT || 27017
-const dbUrl = process.env.DB_URL || "localhost"
-const dbCollection = process.env.DB_COLLECTION || "auth-test";
+    // const dbPort = process.env.DB_PORT || 27017
+    // const dbUrl = process.env.DB_URL || "localhost"
+    // const dbCollection = process.env.DB_COLLECTION || "auth-test";
 
-mongoose.set('useCreateIndex', true);
-mongoose.connect(`mongodb://${dbUrl}/${dbCollection}`, { useNewUrlParser: true })
+// mongoose.set('useCreateIndex', true);
+// mongoose.connect(`mongodb://${dbUrl}/${dbCollection}`, { useNewUrlParser: true })
+//     .then(_ => console.log('Connected Successfully to MongoDB'))
+//     .catch(err => console.error(err))
+
+var opts = {
+    server: {
+        socketOptions: { keepAlive: 1 }
+    }
+};
+const MONGO_URL = 'mongodb://heroku_sj2clvbr:kac225p7edbvo5i8t4nj5bqrcl@ds259586.mlab.com:59586/heroku_sj2clvbr';
+mongoose.set('useCreateIndex', true)
+mongoose.connect(MONGO_URL, { useNewUrlParser: true })
     .then(_ => console.log('Connected Successfully to MongoDB'))
     .catch(err => console.error(err))
 
